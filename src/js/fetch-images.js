19d34d9ebdad21bde5f58ced25c9713462d5d9e7
Drummberg <drummberg@gmail.com>
2022-02-20 02:23:05 +0200

    Update fetch-images.js

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -4,7 +4,7 @@ axios.defaults.baseURL = 'https://pixabay.com/api/'
 
 const KEY = '25704101-e9a4b1030f151f3422020504c'
 
-async function fetchImages(query, page, Perpage) {
+async function fetchImages(query, page, perPage) {
     const response = await axios.get(`?key=${KEY}&q=${query}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=${perPage}`)
 
     return response
